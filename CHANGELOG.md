5aeb24459e653fadc2490669de6eee250ee0bacf
Andriy Zinevych <Andriy.Zinevych@akqa.com>
2021-02-25 13:28:02 +0200

    init commit

---
+++ CHANGELOG.md
@@ -0,0 +1,6 @@
+#### 0.0.1 (2021-02-25)
+
+##### Chores
+
+*  Cleaned up code to stabilize build. ([7e7e70f7](URL_HERE/commit/7e7e70f7e76675c757c2b84c78e5db927efff4fd))
+
---
+++ jest.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  roots: ['src'],
+  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  transform: {
+    '^.+\\.(ts|tsx)$': 'ts-jest'
+  }
+};
